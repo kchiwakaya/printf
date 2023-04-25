164575fcae959ab48b38a3dcd716ca630094ffa5
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-04-25 22:27:14 +0200

    count string

--- put_string.c
+++ put_string.c
@@ -1,6 +1,9 @@
 #include <unistd.h>
 #include "main.h"
-
+/**
+*put_string - returns the number of chars
+*@strinf: the string
+*Return: number of characters in string
 int put_string(char *strinf)
 {
     int index = 0,count = 0;
