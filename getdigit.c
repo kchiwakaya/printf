0f49c6212361164d046e42d7f967fa62421eda40
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-04-25 22:24:53 +0200

    get digit

--- getdigit.c
+++ getdigit.c
@@ -1,19 +1,21 @@
 #include <unistd.h>
 #include "main.h"
+/**
+*getdigit: returns a digit print
+*@number: input digit
+*Returns: integer count
+*/
 
 int getdigit(int number)
 {
-    int n;
+    int holder;
     int count = 0;
-
 
     if (number == 0)
         return(0);
-    n = number/10;
-    getdigit(n);
+    holder = number/10;
+    getdigit(holder);
     _putchar(number%10 + '0');
     count += 1;
     return (count);
-
 }
-
